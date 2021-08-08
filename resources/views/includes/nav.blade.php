0dd9031f8050d1834f7a451e1884ded768313d75
Jelle <jellemaarten02@gmail.com>
2021-08-08 22:08:45 +0200

    laravel install + begin nav

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,21 @@
+<header id="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <div class="branding">
+                    <h3>Jelle Stekelenburg</h3>
+                </div>
+            </div>
+            <div class="col-lg-9 col-6">
+                <div class="menu">
+                    <ul class="menu-parent">
+                        <li class="menu-item"><a href="/">Home</a></li>
+                        <li class="menu-item"><a href="/">Over mij</a></li>
+                        <li class="menu-item"><a href="/">Portfolio</a></li>
+                        <li class="menu-item"><a href="/">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
