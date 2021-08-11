893c8774ed4e3e3eb45235ffc2a220aabe0b9679
Jelle <jellemaarten02@gmail.com>
2021-08-11 21:19:37 +0200

    big update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,12 +7,12 @@
             <div class="row height align-items-center justify-content-center">
                 <div class="col-12">
                     <div class="row">
-                        <div class="col-lg-6 col-lg-rm"></div>
-                        <div class="col-lg-6 col-12">
-                            <h1>Jelle Stekelenburg</h1>
-                            <p>Web Developer</p>
+                        <div class="col-12">
+                            <div class="content">
+                                <h1>Jelle <span class="parent">Stek <span class="child">elenburg</span></span></h1>
+                            </div>
                         </div>
-                        <img class="hero-main" src="{{asset('/images/homepage/hero.png   ')}}" alt="">
+                        <div class="trigger"></div>
                     </div>
                 </div>
             </div>
@@ -21,9 +21,38 @@
 
     <section class="about home">
         <div class="container">
-            <div class="row vh-100">
+            <div class="row">
                 <div class="row-12">
+                    <h4>Over mij</h4>
+                    <div class="divider"></div>
+                    <div class="row">
+                        <div class="col-lg-5 col-12 mt-3">
+                            <p>Ik ben Jelle Stekelenburg, een web developer met 3+ jaar ervaring in development. Sinds
+                                mijn 12e ben ik altijd bezig geweest met digitale creatie, al ging het om Youtube
+                                video's maken, Creatief bezig zijn binnen Photoshop of de technieken leren van alles wat
+                                ik deed.</p>
 
+                                <p>Ik zie mijzelf als een gedreven en positieve developer die zichzelf altijd wilt
+                                verbeteren in het geen wat ik doe. Ik neem niet snel genoegen met het geen wat ik maak
+                                en leer mezelf steeds nieuwe technieken aan.</p>
+
+                                <p>Momenteel ben ik bezig met mijn eerste jaar als Software engineer op de Hogeschool van
+                                Amsterdam, maar hier ben ik niet begonnen. Mijn development avonturen zijn begonnen op
+                                het Mediacollege Amsterdam waar ik in 2021 geslaagd ben voor Media-development diploma.
+                                Binnen deze drie-jarige opleiding heb ik leren werken met html, php, Javascript, (s)css
+                                en frameworks zoals Laravel en Wordpress.</p>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="item"></div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="item"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
