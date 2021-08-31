8923df544d5d4b6921fdef2dd7ade01d204d5894
Jelle <jellemaarten02@gmail.com>
2021-08-31 13:48:29 +0200

    Analytics

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -16,5 +16,14 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="{{asset('/js/main.js')}}"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NXH5Q98JD4"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NXH5Q98JD4');
+</script>
 </body>
 </html>
