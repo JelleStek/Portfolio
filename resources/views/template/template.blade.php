7d653fea94b053cc790f9df711e58e99b68e5916
Jelle <jellemaarten02@gmail.com>
2021-10-11 09:20:27 +0200

    update > Edited loader for faster load time

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('/css/loader.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('/css/main.css')}}">
     <title>Portfolio - Jelle Stekelenburg</title>
@@ -20,17 +21,6 @@
 
     @yield('content')
 </main>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="{{asset('/js/main.js')}}"></script>
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-NXH5Q98JD4"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-NXH5Q98JD4');
-</script>
 <script src="https://unpkg.com/vue@next"></script>
 <script>
     let app = Vue.createApp({
@@ -51,7 +41,18 @@
     function triggerDisplay() {
         let loader = document.getElementById('loader');
         setTimeout(function(){ loader.classList.add('hide'); }, 500);
-    };
+    }
+</script>
+<script defer src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script defer src="{{asset('/js/main.js')}}"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NXH5Q98JD4"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NXH5Q98JD4');
 </script>
 </body>
 </html>
